69df68a8e76bd3598581a8a732fdfe431bf098de
fajarartbekasi <fajarbekasieditting@gmail.com>
2022-04-05 08:17:08 +0700

    healthy

--- resources/views/XII/PR2.blade.php
+++ resources/views/XII/PR2.blade.php
@@ -2,279 +2,116 @@
 
 @section('content')
 
-    <div class="row">
-        <div class="col-md-6">
-            <div class="card border-0">
-                <div class="card-body">
-                    <h4>Store Procedure</h4>
-                    <p>
-                        Hallo untuk materi kali ini kita akan melakukan pembuatan store procedure.
-                        store procedure ini digunakan untuk menyimpan data kedalam table.
-                        saya harap semua design dari index,create,edit,hapus sudah semua kalian buat.
-                        agar kalian dapat mengikuti materi ini.
-                    </p>
-                    <p>
-                        Ok. Kita mulai saja kodingnya.
-                    </p>
-                    <ul class="list-group border-0">
-                        <li class="list-group-item border-0">1.Buka controller yang akan dibuat store prosedur nya</li>
-                        <li class="list-group-item border-0">2.Tambahkan function store dan buat codenya</li>
-                        <li class="list-group-item border-0">3.Buat modelnya</li>
-                        <li class="list-group-item border-0">4.daftarkan model di controller</li>
-                        <li class="list-group-item border-0">5.buka model dan daftarkan tablenya</li>
-                        <li class="list-group-item border-0">6.Buat Route untuk store</li>
-                    </ul>
-                    <p>
-                        example : XampleController.php
-                    </p>
-                    <div class="card ">
-                        <div class="card-body bg-purple-600">
-                            <p>
-                                <h3 class="text-white">store procedure code</h3>
-                                <span class="text-white">
-                                        public function store(Request $request){
-                                            <p>
-                                                $namaVariable = Model::create([
-                                            </p>
-                                            <p>
-                                                'filed1' => $request->field1,
-                                            </p>
-                                            <p>
-                                                ]);
-                                            </p>
-                                            <p>
-                                                    return redirect()->back();
-                                            </p>
-                                        }
-                                </span>
-                            </p>
-                        </div>
+<div class="card border-0 pt-3" >
+    <div class="card-body">
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-10">
+                <div class="alert bg-violet-900" role="alert" style="margin-top: -50px">
+                    <div class="d-flex">
+                        <h2 class="text-white">Hello,</h2>
+                        <h2 class="text-cyan-400">Username</h2>
                     </div>
-                    <p>
-                        Membuat Model
-                    </p>
-                    <div class="card">
-                        <div class="card-body bg-purple-600">
-                            <p class="text-white">
-                                jalankan perintah ini di cmd
-                            </p>
-                            <p class="text-white">
-                               php artisan make:model Example
-                            </p>
-                        </div>
-                    </div>
-                    <p>
-                       example : XampleController.php
-                    </p>
-                    <div class="card">
-                        <div class="card-body bg-purple-600">
-                            <p class="text-white">
-                                namespace App\Http\Controllers;
-                            </p>
-                            <p class="text-white">
-                                use App\Example;
-                            </p>
-                        </div>
-                    </div>
-                    <p>
-                       example : Example.php
-                    </p>
-                    <div class="card">
-                        <div class="card-body bg-purple-600">
-                            <p class="text-white">
-                                protected $table = 'exampletablename';
-                            </p>
-                            <p class="text-white">
-                                protected $guarded = [];
-                            </p>
-                        </div>
-                    </div>
-                    <p>
-                       example : web.php
-                    </p>
-                    <div class="card">
-                        <div class="card-body bg-purple-600">
-                            <p class="text-white">
-                                route::post('store','StoreController@store');
-                            </p>
-                        </div>
-                    </div>
+                    <h6 class="text-white">
+                        What will you do today, if there is a schedule you want to change you can set your in app preferences here.
+                    </h6>
                 </div>
             </div>
         </div>
-        <div class="col-md-6">
-            <div class="card border-0">
-                <div class="card-body">
-                    <h4>Menampilkan Data dari DB</h4>
-                    <p>
-                        Hallo untuk materi kali ini kita akan melakukan proces untuk menampilkan data dari
-                        db ke dalam view html kita
-                    </p>
-                    <p>
-                        Ok. Kita mulai saja kodingnya.
-                    </p>
-                    <ul class="list-group border-0">
-                        <li class="list-group-item border-0">1.Buka controller yang akan dipanggil datanya</li>
-                        <li class="list-group-item border-0">2.modifikasi sedikit di function index</li>
-                        <li class="list-group-item border-0">3.buka index yang ingin di panggil datanya</li>
-                        <li class="list-group-item border-0">4.tambahkan code nya</li>
-                    </ul>
-                    <p>
-                        example : XampleController.php
-                    </p>
-                    <div class="card ">
-                        <div class="card-body bg-purple-600">
-                            <p>
-                                <h3 class="text-white">Modifikasi index</h3>
-                                <span class="text-white">
-                                    <p class="text-white">
-                                       $namavariables = Model::all();
-                                    </p>
-                                    <p class="text-white">
-                                       return view('index' compact('namavariables'));
-                                    </p>
-                                </span>
-                            </p>
-                        </div>
-                    </div>
-                    <p>
-                        example : index.blade.php
-                    </p>
-                    <div class="card ">
-                        <div class="card-body bg-purple-600">
-                             <img src="{{asset('img/foreach.png')}}" alt="">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card-body shadow-sm">
+                    <div class="d-flex align-items-center">
+                        <div>
+                            <h4>Important tasks</h4>
+                            <h6 class="text-secondary">Today 24, Jan 2019</h6>
                         </div>
                     </div>
 
-                </div>
-            </div>
-        </div>
-        <div class="col-md-6 pt-2">
-            <div class="card border-0">
-                <div class="card-body">
-                    <h4>Edit</h4>
-                    <p>
-                        Hallo untuk materi kali ini kita akan melakukan proces untuk mengedit data yang telah kita buat
-                    </p>
-                    <p>
-                        Ok. Kita mulai saja kodingnya.
-                    </p>
-                    <ul class="list-group border-0">
-                        <li class="list-group-item border-0">1.Buka controller yang akan dipanggil edit datanya</li>
-                        <li class="list-group-item border-0">2.modifikasi sedikit di function edit</li>
-                        <li class="list-group-item border-0">3.buka edit.blade yang ingin di edit</li>
-                        <li class="list-group-item border-0">4.modifikasi</li>
-                        <li class="list-group-item border-0">4.tambahkan route di web</li>
-                    </ul>
-                    <p>
-                        example : XampleController.php
-                    </p>
-                    <div class="card ">
-                        <div class="card-body bg-purple-600">
-                            <p>
-                                <h3 class="text-white">Modifikasi edit</h3>
-                                <span class="text-white">
-                                    <p class="text-white">
-                                       public function edit($id)
-                                    </p>
-                                    <p class="text-white">
-                                       $namavariable = Example::findOrFaill($id);
-                                    </p>
-                                    <p class="text-white">
-                                       return view('edit', compact('namavariable'));
-                                    </p>
-                                </span>
-                            </p>
+                    <div class="d-flex justify-content-between">
+                        <h1 class="text-violet-900">45</h1>
+                        <div>
+                            <h6>Urgent</h6>
+                            <h6>Last Urgent</h6>
+                            <h6>Required</h6>
                         </div>
-                    </div>
-                    <p>
-                        example : edit.blade.php
-                    </p>
-                    <div class="card ">
-                        <div class="card-body bg-purple-600">
-                             <img src="{{asset('img/edit.png')}}" width="100%" alt="">
-                             <p class="text-white">
-                                dan tambahkan @method('PATCH') tepat dibawah @csrf
-                             </p>
-                        </div>
-                    </div>
-                    <p>
-                        example : web.php
-                    </p>
-                    <div class="card ">
-                        <div class="card-body bg-purple-600">
-                             <p class="text-white">
-                                route::get('edit\{example}','StoreController@edit');
-                             </p>
-                        </div>
+
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="col-md-6 pt-2">
-            <div class="card border-0">
-                <div class="card-body">
-                    <h4>Update</h4>
-                    <p>
-                        Hallo untuk materi kali ini kita akan melakukan proces untuk Update data yang telah di edit
-                    </p>
-                    <p>
-                        Ok. Kita mulai saja kodingnya.
-                    </p>
-                    <ul class="list-group border-0">
-                        <li class="list-group-item border-0">1.Buka controller yang akan dipanggil update datanya</li>
-                        <li class="list-group-item border-0">2.tambahkan function update</li>
-                        <li class="list-group-item border-0">3.buka edit.blade.php</li>
-                        <li class="list-group-item border-0">4.tambahkan route di edit.blade</li>
-                        <li class="list-group-item border-0">4.tambahkan route di web</li>
-                    </ul>
-                    <p>
-                        example : XampleController.php
-                    </p>
-                    <div class="card ">
-                        <div class="card-body bg-purple-600">
-                            <p>
-                                <h3 class="text-white">Buat function update</h3>
-                                <span class="text-white">
-                                    <p class="text-white">
-                                       public function update(Request $request, $id)
-                                    </p>
-                                    <p class="text-white">
-                                        $namavarialbe = Model::where('id', $id)->first();
-                                    </p>
-                                    <p class="text-white">
-                                       $namavarialbe->field1 = $request->input('field1');
-                                    </p>
-                                    <p class="text-white">
-                                       $namavarialbe->save();
-                                    </p>
-                                </span>
-                            </p>
+            <div class="col-md-4">
+                <div class="card-body shadow-sm">
+                    <div class="d-flex align-items-center">
+                        <div>
+                            <h4>Important tasks</h4>
+                            <h6 class="text-secondary">Today 24, Jan 2019</h6>
                         </div>
                     </div>
-                    <p>
-                        example : edit.blade.php
-                    </p>
-                    <div class="card ">
-                        <div class="card-body bg-purple-600">
-                             <p class="text-white">
-                               route('update',$get->id)
-                             </p>
+
+                    <div class="d-flex justify-content-between">
+                        <h1 class="text-violet-900">45</h1>
+                        <div>
+                            <h6>Urgent</h6>
+                            <h6>Last Urgent</h6>
+                            <h6>Required</h6>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card-body shadow-sm">
+                    <div class="d-flex align-items-center">
+                        <div>
+                            <h4>Important tasks</h4>
+                            <h6 class="text-secondary">Today 24, Jan 2019</h6>
                         </div>
                     </div>
-                    <p>
-                        example : web.php
-                    </p>
-                    <div class="card ">
-                        <div class="card-body bg-purple-600">
-                             <p class="text-white">
-                                route::pacth('update\{example}','StoreController@update');
-                             </p>
+
+                    <div class="d-flex justify-content-between">
+                        <h1 class="text-violet-900">45</h1>
+                        <div>
+                            <h6>Urgent</h6>
+                            <h6>Last Urgent</h6>
+                            <h6>Required</h6>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <div class="pt-3 row mx-auto">
+                <div class="col-md-3">
+                    <div class="card-body shadow-sm">
+                        <div class="text-center">
+                            <h4 class="text-cyan-400">Laboratory</h4>
+                            <h6 class="text-secondary">Result</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card-body shadow-sm">
+                        <div class="text-center">
+                            <h4 class="text-cyan-400">New Event</h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card-body shadow-sm">
+                        <div class="text-center">
+                            <h4 class="text-cyan-400">Reprt</h4>
+                            <h6 class="">Archives</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card-body shadow-sm">
+                        <div class="text-center">
+                            <h4 class="text-cyan-400">Payments</h4>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,10 +22,14 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+        <nav class="navbar navbar-expand-md navbar-light bg-violet-900 navbar-laravel">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+
+                <a class="navbar-brand text-white" href="{{ url('/') }}">
+                    <div class="d-flex">
+                        <h3>HEALTHY</h3>
+                        <h3 class="text-cyan-400">KIT</h3>
+                    </div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -34,47 +38,49 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('XII') }}">{{ __('Store') }}</a>
-                        </li>
+
                     </ul>
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        <div class="d-flex align-items-center">
+                            <li class="nav-item mr-3">
+                                <span>
+                                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="M10 2C6.68632 2 4.00003 4.68629 4.00003 8V11.5858L3.29292 12.2929C3.00692 12.5789 2.92137 13.009 3.07615 13.3827C3.23093 13.7564 3.59557 14 4.00003 14H16C16.4045 14 16.7691 13.7564 16.9239 13.3827C17.0787 13.009 16.9931 12.5789 16.7071 12.2929L16 11.5858V8C16 4.68629 13.3137 2 10 2Z"
+                                            fill="white" />
+                                        <path d="M10 18C8.34315 18 7 16.6569 7 15H13C13 16.6569 11.6569 18 10 18Z" fill="white" />
+                                    </svg>
+                                </span>
                             </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                            <li class="nav-item d-flex align-items-center">
+                                <img src="{{asset('img/profile.jpg')}}" alt="" srcset="" class="rounded-full">
+                                <a href="" class="nav-link text-white">
+                                    Username
                                 </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
                             </li>
-                        @endguest
+                        </div>
+
                     </ul>
                 </div>
             </div>
         </nav>
-
+        <div class="nav-scroller bg-white shadow-sm">
+            <nav class="nav nav-underline container" aria-label="Secondary navigation">
+                <a class="nav-link text-violet-900 active" aria-current="page" href="#">Dashboard</a>
+                <a class="nav-link text-violet-900" href="#">
+                    Friends
+                </a>
+                <a class="nav-link text-violet-900" href="#">Dashboard</a>
+                <a class="nav-link text-violet-900" href="#">Component</a>
+                <a class="nav-link text-violet-900" href="#">Appoitments</a>
+                <a class="nav-link text-violet-900" href="#">Doctors</a>
+                <a class="nav-link text-violet-900" href="#">Patients</a>
+                <a class="nav-link text-violet-900" href="#">Messages</a>
+                <a class="nav-link text-violet-900" href="#">Settings</a>
+            </nav>
+        </div>
         <main class="py-4">
             <div class="container">
                 @yield('content')
